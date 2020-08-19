b812ac728b37148f659fad3fcf5b2723041bf92a
Mary Schmidt <maryschmidt1982@gmail.com>
2020-08-19 15:10:46 -0500

    mvp implementation

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function formatQueryParams(searchTerm) {
 
 function displayResults(responseJson) {
   $("#results-list").append(
-    `<li><p>Positive Test Results:</p>${responseJson.positive}</li>
-        <li><p>Negative Test Results:</p>${responseJson.negative}</li>
-        <li><p>Confirmed Deaths:</p>${responseJson.deathConfirmed}</li>
+    `<li><p>Positive Test Results:</p>${responseJson.positive}<p>Total number of poeple with confirmed or probable cases of Covid-19. A confirmed case has a positive test result, a probably case is when a patinent has symptoms and has been exposed to a confirmed case.</li>
+        <li><p>Negative Test Results:</p>${responseJson.negative}<p>Results of the PCR test tha have returned negative. This number should be considered an estimate because of the complexity of reporting it.</li>
+        <li><p>Confirmed Deaths:</p>${responseJson.deathConfirmed}<p>Total fatalities with confirmed Covid-19 diagnosis and the death certificate lists Covid-19 as a cause or underlying cause of death.</li>
         `
   );
 }
