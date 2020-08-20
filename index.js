96fa760cdd32c407ef9813c54accfaadad97158a
Mary Schmidt <maryschmidt1982@gmail.com>
2020-08-20 14:16:21 -0500

    css initial upload js new numberwithcomma function

--- index.js
+++ index.js
@@ -3,23 +3,32 @@
 // put your own value below!
 //const apiKey = "47cpKe28N0zP9QP2YJtAgmNiiQCdVicOMrk3f295";
 const searchURL = "https://api.covidtracking.com";
+const covidURL = "https://corona-api.com/countries:/code/US";
+
+console.log(covidURL);
 
 function formatQueryParams(searchTerm) {
   console.log(searchTerm);
   //const stateArr = searchTerm.map();
 }
 
+function numberWithCommas(x) {
+  if (!x) {
+    return "Data not available";
+  }
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 function displayResults(responseJson) {
+  const positiveNum = numberWithCommas(responseJson.positive);
+  const negativeNum = numberWithCommas(responseJson.negative);
+  const confirmedDeath = numberWithCommas(responseJson.deathConfirmed);
   $("#results-list").append(
-    `<li><p>Positive Test Results:</p>${responseJson.positive}<p>Total number of poeple with confirmed or probable cases of Covid-19. A confirmed case has a positive test result, a probably case is when a patient has symptoms and has been exposed to a confirmed case.</li>
-        <li><p>Negative Test Results:</p>${responseJson.negative}<p>Results of the PCR (molecular) test the have returned negative. This number should be considered an estimate because of the complexity of reporting it.</li>
-        <li><p>Confirmed Deaths:</p>${responseJson.deathConfirmed}<p>Total fatalities with confirmed Covid-19 diagnosis and the death certificate lists Covid-19 as a cause or underlying cause of death.</li>
+    `<li><p>Positive Test Results:</p>${positiveNum}<p>Total number of poeple with confirmed or probable cases of Covid-19. A confirmed case has a positive test result, a probably case is when a patient has symptoms and has been exposed to a confirmed case.</li>
+        <li><p>Negative Test Results:</p>${negativeNum}<p>Results of the PCR (molecular) test the have returned negative. This number should be considered an estimate because of the complexity of reporting it.</li>
+        <li><p>Confirmed Deaths:</p>${confirmedDeath}<p>Total fatalities with confirmed Covid-19 diagnosis and the death certificate lists Covid-19 as a cause or underlying cause of death.</li>
         `
   );
-  //   function numberWithCommas(num) {
-  //     var num = displayResults(responseJson);
-  //     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  //   }
 }
 
 // iterate through the items array
@@ -40,7 +49,7 @@ function getData(searchTerm) {
     .then((responseJson) => displayResults(responseJson))
     .catch((err) => {
       $("#js-error-message").text(
-        `Enter state code, example: MN or mn ${err.message}`
+        `Select an option from dropdown menu${err.message}`
       );
     });
 }
