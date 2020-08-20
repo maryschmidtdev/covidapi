a6b3b787a2f640db277604b56e0c339d4406a71d
Mary Schmidt <maryschmidt1982@gmail.com>
2020-08-20 11:39:42 -0500

    changed to dropdown menu

--- index.js
+++ index.js
@@ -6,16 +6,20 @@ const searchURL = "https://api.covidtracking.com";
 
 function formatQueryParams(searchTerm) {
   console.log(searchTerm);
-  //   const stateArr = searchTerm.map();
+  //const stateArr = searchTerm.map();
 }
 
 function displayResults(responseJson) {
   $("#results-list").append(
-    `<li><p>Positive Test Results:</p>${responseJson.positive}<p>Total number of poeple with confirmed or probable cases of Covid-19. A confirmed case has a positive test result, a probably case is when a patinent has symptoms and has been exposed to a confirmed case.</li>
-        <li><p>Negative Test Results:</p>${responseJson.negative}<p>Results of the PCR test tha have returned negative. This number should be considered an estimate because of the complexity of reporting it.</li>
+    `<li><p>Positive Test Results:</p>${responseJson.positive}<p>Total number of poeple with confirmed or probable cases of Covid-19. A confirmed case has a positive test result, a probably case is when a patient has symptoms and has been exposed to a confirmed case.</li>
+        <li><p>Negative Test Results:</p>${responseJson.negative}<p>Results of the PCR (molecular) test the have returned negative. This number should be considered an estimate because of the complexity of reporting it.</li>
         <li><p>Confirmed Deaths:</p>${responseJson.deathConfirmed}<p>Total fatalities with confirmed Covid-19 diagnosis and the death certificate lists Covid-19 as a cause or underlying cause of death.</li>
         `
   );
+  //   function numberWithCommas(num) {
+  //     var num = displayResults(responseJson);
+  //     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  //   }
 }
 
 // iterate through the items array
