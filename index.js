04617b760be52d875fa9580365da035a52c7cabc
Mary Schmidt <maryschmidt1982@gmail.com>
2020-08-24 21:45:56 -0500

    2 apis working

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ function displayResults(responseJson) {
   const negativeNum = numberWithCommas(responseJson.negative);
   const confirmedDeath = numberWithCommas(responseJson.deathConfirmed);
   $("#results-list").append(
-    `<li><h2>Positive Test Results:</h2>${positiveNum}<p>Total number of poeple with confirmed or probable cases of Covid-19. A confirmed case has a positive test result, a probably case is when a patient has symptoms and has been exposed to a confirmed case.</li>
-        <li><h2>Negative Test Results:</h2>${negativeNum}<p>Results of the PCR (molecular) test the have returned negative. This number should be considered an estimate because of the complexity of reporting it.</li>
-        <li><h2>Confirmed Deaths:</h2>${confirmedDeath}<p>Total fatalities with confirmed Covid-19 diagnosis and the death certificate lists Covid-19 as a cause or underlying cause of death.</li>
+    `<li><h2>Positive Test Results:</h2> ${positiveNum}<p>Total number of poeple with confirmed or probable cases of Covid-19. A confirmed case has a positive test result, a probably case is when a patient has symptoms and has been exposed to a confirmed case.</li>
+        <li><h2>Negative Test Results:</h2> ${negativeNum}<p>Results of the PCR (molecular) test the have returned negative. This number should be considered an estimate because of the complexity of reporting it.</li>
+        <li><h2>Confirmed Deaths:</h2> ${confirmedDeath}<p>Total fatalities with confirmed Covid-19 diagnosis and the death certificate lists Covid-19 as a cause or underlying cause of death.</li>
         `
   );
 
@@ -41,12 +41,46 @@ function getData(searchTerm) {
     });
 }
 
+const covidActNowApi = "https://data.covidactnow.org";
+
+function stateProjections(responseJson) {
+  //const date = numberWithCommas(responseJson.metrics.date);
+  const rt = responseJson.projections.rt;
+  const testPositivity = responseJson.metrics.testPositivity;
+  $("#results-list").append(
+    `<li><h2>Rt:</h2> ${rt}<p>Rate of infection, for example, how many people the virus could spread to from one infected person.</p></li>
+          <li><h2>Test Positivity Ratio:</h2> ${testPositivity}<p>Ratio of people who test positive calculated using a 7-day rolling average.</p></li>`
+  );
+}
+
+function projectedData(searchTerm) {
+  const covidactnow =
+    covidActNowApi +
+    "/latest/us/states/" +
+    searchTerm +
+    ".OBSERVED_INTERVENTION.json";
+
+  fetch(covidactnow)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then((responseJson) => stateProjections(responseJson))
+
+    .catch((err) => {
+      $("#js-error-message").text(`Data not available${err.message}`);
+    });
+}
+
 function watchForm() {
   $("form").submit((event) => {
     event.preventDefault();
     $("#results-list").empty();
     const searchTerm = $("#js-search-term").val();
     getData(searchTerm);
+    projectedData(searchTerm);
   });
 }
 
@@ -69,30 +103,10 @@ function usData() {
       throw new Error(response.statusText);
     })
     .then((responseJson) => usResults(responseJson))
-    // remove console.log
-    // call usResults and pass in responseJson
     .catch((err) => {
       $("#js-error-message").text(`Data not available${err.message}`);
     });
 }
 usData();
 
-function projectedData() {
-  const covidActNowApi =
-    "https://data.covidactnow.org/latest/us/states/CA.OBSERVED_INTERVENTION.json";
-  fetch(covidActNowApi)
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(response.statusText);
-    })
-    .then((responseJson) => usResults(responseJson))
-    // remove console.log
-    // call usResults and pass in responseJson
-    .catch((err) => {
-      $("#js-error-message").text(`Data not available${err.message}`);
-    });
-}
-projectedData();
 $(watchForm);
